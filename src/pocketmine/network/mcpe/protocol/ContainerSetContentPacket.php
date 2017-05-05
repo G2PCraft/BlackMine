10e0f3225a8d65d48c87e19821c1896eef3ec4d9
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-05-05 13:49:35 +0200

    fix

--- src/pocketmine/network/mcpe/protocol/ContainerSetContentPacket.php
+++ src/pocketmine/network/mcpe/protocol/ContainerSetContentPacket.php
@@ -28,7 +28,7 @@ use pocketmine\network\mcpe\NetworkSession;
 
 class ContainerSetContentPacket extends DataPacket{
 
-	const NETWORK_ID = Info::CONTAINER_SET_CONTENT_PACKET;
+	const NETWORK_ID = ProtocolInfo::CONTAINER_SET_CONTENT_PACKET;
 
 	const SPECIAL_INVENTORY = 0;
 	const SPECIAL_ARMOR = 0x78;
