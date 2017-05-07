ca39e5f89462296b76a1137b5200eb0428875161
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-05-07 10:26:34 +0200

    litle by litle steps

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -104,6 +104,7 @@ use pocketmine\utils\TextFormat;
 use pocketmine\utils\Utils;
 use pocketmine\utils\UUID;
 use pocketmine\utils\VersionString;
+use pocketmine\network\protocol\Info;
 
 /**
  * The class that manages everything
